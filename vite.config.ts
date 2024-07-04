ddabfd6a6a625affeddd19ccea456392e994bc87
Ouka <ouka2011.oc@gmail.com>
2024-07-04 22:57:13 +0800

    update.

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  plugins: [
+    dts(),
+    viteStaticCopy({
+      targets: [{ src: 'src/client.d.ts', dest: '' }]
+    })
+  ],
+  build: {
+    lib: {
+      entry: ['src/index.ts'],
+      name: 'vite-plugin-html-string-minify',
+      formats: ['es', 'cjs']
+    },
+    rollupOptions: { external: ['vite', 'minify'] }
+  }
+})
